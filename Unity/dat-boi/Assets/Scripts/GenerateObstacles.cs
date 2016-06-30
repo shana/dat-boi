cff063ef542cd332a03cc159678c99fdeffcc67a
Jasmine <paladique@github.com>
2016-06-30 18:11:39 +0200

    add missed script

---
+++ Unity/dat-boi/Assets/Scripts/GenerateObstacles.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateObstacles : MonoBehaviour {
+
+    public Transform cube;
+
+	// Use this for initialization
+	void Start () {
+
+        for(int y = 0; y < 5; y++)
+        {
+            for(int x = 0; x < 5; x++)
+            {
+                Instantiate(cube, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
